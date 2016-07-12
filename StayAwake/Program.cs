54b0c60a07c19416c8f8e0d77ab2fbec546e947c
bryanknox <bryan@knoxbits.com>
2016-07-12 15:56:28 -0700

    Added basic command line exception handling.

--- StayAwake/Program.cs
+++ StayAwake/Program.cs
@@ -8,8 +8,58 @@ namespace StayAwake
 {
 	class Program
 	{
+		#region Main
+
 		static void Main(string[] args)
 		{
+			Console.WriteLine();
+			Console.WriteLine("StayAwake");
+			Console.WriteLine();
+
+			try
+			{
+				// TODO: do stuff.
+
+			}
+			catch (Exception ex)
+			{
+				OutputExceptionInfo(ex);
+			}
+
+			Console.WriteLine();
+			Console.Write("\nPress ENTER to EXIT...");
+			Console.ReadLine();
 		}
+
+		#endregion
+		#region Private Exception Output Methods
+
+		private static void OutputExceptionInfo(Exception ex)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("Exception Caught.");
+			Console.WriteLine(ex.GetType().ToString());
+			Console.WriteLine(ex.Message);
+			if (ex.InnerException != null)
+			{
+				OutputInnerExceptionInfoRecursive(ex.InnerException);
+			}
+		}
+
+		private static void OutputInnerExceptionInfoRecursive(Exception ex)
+		{
+			Console.WriteLine("");
+			Console.WriteLine("Inner");
+			Console.WriteLine(ex.GetType().ToString());
+			Console.WriteLine(ex.Message);
+
+			if (ex.InnerException != null)
+			{
+				// Recurse.
+				OutputInnerExceptionInfoRecursive(ex.InnerException);
+			}
+		}
+
+		#endregion
 	}
 }
